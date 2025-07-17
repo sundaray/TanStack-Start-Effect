ada917327f22973deabed9d1bab34c9c33d7cb31
Hemanta Sundaray <pgcim14.hemant@spjimr.org>
2025-07-17 14:24:08 +0530

    removed extrac ts config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import viteTsConfigPaths from "vite-tsconfig-paths";
-import tsConfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
 export default defineConfig({
@@ -8,7 +7,6 @@ export default defineConfig({
     port: 3000,
   },
   plugins: [
-    tsConfigPaths(),
     tanstackStart(),
     viteTsConfigPaths({
       projects: ["./tsconfig.json"],
