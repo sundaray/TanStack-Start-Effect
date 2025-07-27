a4be9abebe86e830886c1cfbd26b1ca122dc1048
Hemanta Sundaray <pgcim14.hemant@spjimr.org>
2025-07-27 14:21:23 +0530

    spacing updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,3 @@
-// tailwind.config.js
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   theme: {
@@ -7,17 +5,9 @@ module.exports = {
       typography: {
         sm: {
           css: {
-            // We are no longer targeting 'li' directly.
-            // Instead, we are targeting the 'p' inside the 'li'.
             "li > p": {
               marginTop: "0.25rem",
               marginBottom: "0.25rem",
-            },
-            // It's also good practice to reset the li's own margin,
-            // just in case, to ensure the p is the only thing creating space.
-            li: {
-              marginTop: "0",
-              marginBottom: "0",
             },
           },
         },
