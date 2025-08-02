2a85eba86553c57e3cf7e0546ff4028a2f8fb6c1
Hemanta Sundaray <pgcim14.hemant@spjimr.org>
2025-08-02 16:04:04 +0530

    tool upload schema

--- src/routes/submit.tsx
+++ src/routes/submit.tsx
@@ -6,6 +6,12 @@ export const Route = createFileRoute("/submit")({ component: Submit });
 export default function Submit() {
   return (
     <div className="max-w-lg mx-auto py-8">
+      <h1 className="text-4xl tracking-tight text-neutral-900 font-semibold text-center">
+        Submit your AI tool
+      </h1>
+      <h2 className="text-2xl text-neutral-500 text-center">
+        Get a do-follow backlink for free.
+      </h2>
       <ToolSubmissionForm />
     </div>
   );
